045bce77560b4dfd8f644433fff453348dd1c84b
fatehuanchen <1540226204@qq.com>
2016-12-07 15:31:36 +0800

    hello

---
+++ Testjihe/src/test/JiheTest.java
@@ -0,0 +1,15 @@
+package test;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JiheTest {
+
+	public static void main(String[] args) {
+		String str="420";
+		str+=42;
+		System.out.print(str);
+	}
+
+}
